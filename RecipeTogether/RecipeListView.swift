84989723bceb3c31bb751c5725c8fb63c03f9635
Luis Angel <luisangelquinteroarias@192.168.1.6>
2025-05-16 08:06:51 -0500

    Add Initital structure for recipeList

--- RecipeTogether/RecipeListView.swift
+++ RecipeTogether/RecipeListView.swift
@@ -9,7 +9,44 @@ import SwiftUI
 
 struct RecipeListView: View {
     var body: some View {
-        Text("Hello, World from RecipeListView!")
+        VStack{
+            HStack{
+                Text("Hello Alex")
+                    .font(.largeTitle)
+                Image("ProfileImages")
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .frame(width: 120, height: 120)
+                
+            }
+            VStack{
+                Text("Recepies:")
+                    .font(.largeTitle)
+                VStack{                VStack{
+                    Text("Spaghettie")
+                    Text("Difficulty: Hard")
+                    HStack{
+                        Text("Number of ingredientes: 10")
+                        Text("Effort: Hight")
+                    }
+                    
+                }
+                    VStack{
+                        Text("Eggs")
+                        Text("Difficulty: Easy")
+                        HStack{
+                            Text("Number of ingredientes: 2")
+                            Text("Effort: Low")
+                        }
+                        
+                    }
+                }
+
+                
+            }
+            
+        }
     }
 }
 
