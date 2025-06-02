e077f08a353ac2f88270e94063d88a0c74d82db3
Luis Angel <luis-angelquintero@hotmail.com>
2025-06-02 14:01:56 -0500

    Add initial form to AddRecipeView

--- RecipeTogether/Views/AddRecipeView.swift
+++ RecipeTogether/Views/AddRecipeView.swift
@@ -8,8 +8,53 @@
 import SwiftUI
 
 struct AddRecipeView: View {
+    
+    @State private var title = ""
+    @State private var ingredients = ""
+    @State private var instructions = ""
+    @State private var difficulty = "Easy"
+    
+    let difficultyOptions: [String] = ["Easy", "Medium", "Hard"]
+    
     var body: some View {
-        Text("AddRecipeView")
+        NavigationView {
+            Form {
+                Section(header: Text("Recipe Info:")) {
+                    TextField("Title", text: $title)
+                    TextField("Ingredients", text: $ingredients)
+                    Picker("Difficulty", selection: $difficulty) {
+                        ForEach(difficultyOptions, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section(header: Text("Instructions:")) {
+                    TextEditor(text: $instructions)
+                        .frame(minHeight: 150)
+                }
+                
+                Section (header: Text("Estimated time (minutes):")){
+                    HStack {
+                        TextField("time", text: $title)
+                    }
+                }
+                
+                Section {
+                    Button(action: {
+                        submitRecipe()
+                    }) {
+                        Text("Add Recipe")
+                    }
+                }
+            }
+        }
+        .navigationTitle("Add Recipe")
+    }
+    
+    func submitRecipe() {
+        print("Recipe submitted: \(title), \(difficulty), effort:")
+        // ToDo integrate API or local DB saving logic
     }
 }
 
