72b449785a47ef13cf6146facf6c9eb7623ccd48
Luis Angel <luis-angelquintero@hotmail.com>
2025-06-26 07:59:28 -0500

    add the function: uploadImage

--- RecipeTogether/Views/AddRecipeView.swift
+++ RecipeTogether/Views/AddRecipeView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct AddRecipeView: View {
     
@@ -60,6 +61,50 @@ struct AddRecipeView: View {
         }
     }
     
+    
+    func uploadImage(_ image: UIImage) async throws -> String {
+        guard let url = URL(string: "http://localhost:8000/upload-image") else {
+            throw URLError(.badURL)
+        }
+        
+        let boundary = UUID().uuidString
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        
+        let imageData = image.jpegData(compressionQuality: 0.8) ?? Data()
+        var body = Data()
+        
+
+        // Append file
+        body.append("--\(boundary)\r\n")
+        body.append("Content-Disposition: form-data; name=\"file\"; filename=\"image.jpg\"\r\n")
+        body.append("Content-Type: image/jpeg\r\n\r\n")
+        body.append(imageData)
+        body.append("\r\n")
+
+        // Append recipe title
+        body.append("--\(boundary)\r\n")
+        body.append("Content-Disposition: form-data; name=\"recipe_title\"\r\n\r\n")
+        body.append(title)
+        body.append("\r\n")
+
+        body.append("--\(boundary)--\r\n")
+        
+        request.httpBody = body
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw URLError(.badServerResponse)
+        }
+        if let decoded = try? JSONDecoder().decode([String: String].self, from: data),
+           let imageUrl = decoded["url"]{
+            return imageUrl
+        } else {
+            throw URLError(.cannotParseResponse)
+        }
+        
+    }
     func submitRecipe() async throws {
         guard let url = URL(string: "http://localhost:8000/recipes") else {
             print("Invalid URL")
@@ -67,12 +112,14 @@ struct AddRecipeView: View {
         }
         
         let recipeData: [String: Any] = [
+            "id": UUID().uuidString,
             "title": title,
             "difficulty": difficulty,
-            "effort": "f",
-            "minutes": Int(minutes) ?? 0,
             "ingredients": ingredients,
-            "instructions": instructions
+            "minutes": Int(minutes) ?? 0,
+            "instructions": instructions,
+            "imagePath": "",
+            "timestamp": ISO8601DateFormatter().string(from: Date())
         ]
         
         guard let jsonData = try? JSONSerialization.data(withJSONObject: recipeData) else {
