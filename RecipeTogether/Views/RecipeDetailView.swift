1ccc4ef1b08ba05ec127db7de285b8e7b94eb705
Luis Angel <luis-angelquintero@hotmail.com>
2025-06-29 21:57:50 -0500

    Create AsyncImage handler

--- RecipeTogether/Views/RecipeDetailView.swift
+++ RecipeTogether/Views/RecipeDetailView.swift
@@ -11,14 +11,23 @@ struct RecipeDetailView: View {
     
     var body: some View {
         VStack {
-            Text(recipe.title).font(RecipeFonts.title)
-            Image(recipe.title)
-                .resizable()
-                .scaledToFit()
-                .cornerRadius(15)
+            AsyncImage(url: URL(string: "http://127.0.0.1:8000/images/hello_49bff9076ef14dd3944c91ef34e8bfa2.jpg")){ phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } else if phase.error != nil {
+                    Text("Error loading image")
+                } else {
+                    ProgressView()
+                }
+                
+            }
+            .cornerRadius(15)
                 .containerRelativeFrame(.vertical) { size, axis in
                     size * 0.35
                 }
+            Text(recipe.title).font(RecipeFonts.title)
             HStack {
                 
                 Text("Time: \(recipe.minutes)").font(RecipeFonts.body)
