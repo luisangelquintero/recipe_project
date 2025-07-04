8dce443e6be717ecc90a2226dacb970df93e5376
Luis Angel <luis-angelquintero@hotmail.com>
2025-07-03 19:35:53 -0500

    add theme color struct

--- RecipeTogether/Theme/ThemeColors.swift
+++ RecipeTogether/Theme/ThemeColors.swift
@@ -4,4 +4,19 @@
 //
 //  Created by Luis angel Quintero arias on 17/06/25.
 //
+import SwiftUI
+
+struct ThemeColors {
+    static let accent: Color = Color("AccentColor")
+    static let background: Color = Color("BackgroundColor")
+    static let card: Color = Color("CardColor")
+    static let difficultyEasy: Color = Color("DifficultyEasy")
+    static let difficultyHard: Color = Color("DifficultyHard")
+    static let difficultyMedium: Color = Color("DifficultyMedium")
+    static let primary: Color = Color("PrimaryColor")
+    static let secondary: Color = Color("SecondaryColor")
+    static let textPrimary: Color = Color("TextPrimaryColor")
+    static let TextSecondary: Color = Color("TextSecondaryColor")
+}
+    
 
