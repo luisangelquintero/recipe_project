854553dfe3b9ef91649b6d06c6ec019b6f532973
Luis Angel <luis-angelquintero@hotmail.com>
2025-07-07 08:02:13 -0500

    Add struct for ApiConfig

--- RecipeTogether/Configuration/ApiConfig.swift
+++ RecipeTogether/Configuration/ApiConfig.swift
@@ -5,3 +5,17 @@
 //  Created by Luis angel Quintero arias on 7/07/25.
 //
 
+struct ApiConfig {
+    
+    #if DEBUG
+    static let baseUrl = "http://127.0.0.1:8000"
+    #elseif STAGING
+    static let baseUrl = "https://staging.recipetogether.com"
+    #else
+    let baseUrl = "https://api.recipetogether.com"
+    #endif
+    
+    static let imageBaseUrl = "\(baseUrl)/images/"
+    static let recipeEndpint = "\(baseUrl)/recipes"
+    static let uploadImageEndpoint = "\(baseUrl)/upload-image"
+}
