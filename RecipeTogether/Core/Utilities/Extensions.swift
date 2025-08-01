3e48775efa0ca01b371df68510430b72fc65c916
Luis Angel <luis-angelquintero@hotmail.com>
2025-08-01 08:21:57 -0500

    Migrate all the files to the new struct

---
+++ RecipeTogether/Core/Utilities/Extensions.swift
@@ -0,0 +1,7 @@
+//
+//  Extensions.swift
+//  RecipeTogether
+//
+//  Created by Luis angel Quintero arias on 1/08/25.
+//
+
---
+++ RecipeTogether/Core/Utilities/Helpers.swift
@@ -0,0 +1,7 @@
+//
+//  Helpers.swift
+//  RecipeTogether
+//
+//  Created by Luis angel Quintero arias on 1/08/25.
+//
+
