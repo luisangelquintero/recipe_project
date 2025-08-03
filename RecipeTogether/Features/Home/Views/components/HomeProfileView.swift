fb43899b455ed642280369eacf8720ae8c2679ee
Luis Angel <luis-angelquintero@hotmail.com>
2025-08-03 12:56:34 -0500

    Create first conditional struct for homeProfileView

--- RecipeTogether/Features/Home/Views/components/HomeProfileView.swift
+++ RecipeTogether/Features/Home/Views/components/HomeProfileView.swift
@@ -7,17 +7,38 @@
 import SwiftUI
 
 struct HomeProfileView: View {
+    var loggIn : Bool = false
     var body: some View {
         HStack{
-            Text("Hello,  Alex")
-                .foregroundColor(ThemeColors.textPrimary)
-                .font(RecipeFonts.title)
-            Spacer()
-            Image("ProfileImages")
-                .resizable()
-                .scaledToFit()
-                .clipShape(Circle())
-                .frame(width: 100, height: 100)
+            if loggIn {
+                Text("Hello,  Alex")
+                    .foregroundColor(ThemeColors.textPrimary)
+                    .font(RecipeFonts.title)
+                Spacer()
+                Image("ProfileImages")
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .frame(width: 100, height: 100)
+            }else{
+                HStack{
+                    Text("Hi There!")
+                        .foregroundColor(ThemeColors.textPrimary)
+                        .font(RecipeFonts.title)
+                    Spacer()
+                    Button("Login", systemImage: "person.circle.fill"){
+                        print("Login")
+                    }.font(RecipeFonts.button)
+                        .padding(RecipeSpacing.sm)
+                        .background(ThemeColors.primary)
+                        .foregroundStyle(ThemeColors.background)
+                        .clipShape(Capsule())
+                    
+                    
+                }.padding(RecipeSpacing.md)
+            }
+                
+   
         }
     }
 }
